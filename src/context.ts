9c55636229f4567ba2052516650a681bf77885c4
PX-72 <zoli.konyves@pm.me>
2023-03-07 11:53:20 +0000

    just doc format

--- src/context.ts
+++ src/context.ts
@@ -1,10 +1,9 @@
-import { createPortfolio } from "./portfolio.js";
-import { build, append } from "./utils/dom-helper.js";
-import { Context } from "./data/models";
-import { createSignal, createEffect } from "./utils/reactivity.js";
+import { createPortfolio } from './portfolio.js';
+import { build, append } from './utils/dom-helper.js';
+import { Context } from './data/models';
+import { createSignal, createEffect } from './utils/reactivity.js';
 
 export const createContext = (contextData: Context): HTMLElement => {
-
   const [getCounter, setCounter] = createSignal(0);
 
   const counterDiv = build('div');
@@ -14,10 +13,10 @@ export const createContext = (contextData: Context): HTMLElement => {
   });
 
   return append(
-    build("div"),
-    build("p", { text: `Context ID: ${contextData.id}` }),
-    build("p", { text: `Definition: ${contextData.definition}` }),
-    build("p", { text: `Risk Default: ${contextData.isRiskDefault}` }),
+    build('div'),
+    build('p', { text: `Context ID: ${contextData.id}` }),
+    build('p', { text: `Definition: ${contextData.definition}` }),
+    build('p', { text: `Risk Default: ${contextData.isRiskDefault}` }),
     createPortfolio(contextData.portfolio),
     counterDiv,
     build('button', { text: 'Add', eventType: 'click', eventCallback: () => setCounter(getCounter() + 1) })
